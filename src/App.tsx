c5c1f6c6bec57b529d1d0653e25e16de8b9b8d85
GuilhermeFD19 <guii.doreto@gmail.com>
2022-05-03 18:09:39 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Hero } from "./components/Hero"
+import { Widget } from "./components/Widget"
+
+
+export function App() {
+  return (
+    <><Hero /><Widget /></>
+    
+  )
+}
+
+export default App
